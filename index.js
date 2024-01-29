7b30159ae5b064b59e1d5d65b77c204a6c4245b4
claudianeR <claudiane.guapa@gmail.com>
2024-01-29 19:39:58 -0300

    aula node

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const HOST = 'localhost'
+const PORT = 3000
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Resposta Enviada!')                                                                                    
+
+});
+
+server.listen(PORT, HOST, ()=>   {
+    console.log(`Servidor esta rodando em: http://${HOST}:${PORT}`);
+
+})
